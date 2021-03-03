1238659c9458e0c4a939f8b824067e66f39f289d
Kaan Catik <kaancatik@me.com>
2021-03-03 00:11:49 -0800

    Add Celsius or Fahrenheit button, some css features.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,16 @@ import React from 'react'
 export default function SearchBar() {
     return (
         <div className='searchBarInput'>
-            <input type="text" className="resizedTextBox"
-                placeholder={"Search City or Zip Code"}
-            />
-            <button>Search</button>
+            <span className="weatherApp">The Weather App</span>
+            <span>
+                <input type="text" className="resizedTextBox"
+                    placeholder={"Search City or Zip Code"}
+                />
+                <button className="resizedSearchButton">Search</button>
+            </span>
+            <span>
+                <button className="resizedDegreeButton">°C/°F</button>
+            </span>  
         </div>
     )
 }
