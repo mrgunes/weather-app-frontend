9fb4c105f7705780291bfe68fb342ffd8c7aea8a
Kaan Catik <kaancatik@me.com>
2021-03-07 23:18:55 -0800

    Add weather api from openweathermap.org.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function SearchBar() {
+    let [name,setName]=useState('');
+
+    let handleInput=(e)=>{
+        setName(e.target.value)
+    }
+
+    let handleSubmit=()=>{
+        let apiKey='27e977f5956015ec7bb12876112d5dd6';
+        let url=`api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`
+        fetch(url)
+        .then(response=>response.json())
+        .then(json=>console.log(json))
+    }
+
     return (
         <div className='searchBarInput'>
             <span className="weatherApp">The Weather App</span>
