d2b0c39e2f2e93e3e0c83875e9024f9aad9eb9fc
Kaan Catik <kaancatik@me.com>
2021-03-12 19:34:12 -0800

    To backend implemented is completed.

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -10,6 +10,7 @@ export default function SearchBar() {
     let handleInput=(e)=>{
         setName(e.target.value)
     }
+    console.log(name)
 
     let handleUnit=(e)=>{
         setUnit(e.target.value)   
@@ -53,11 +54,11 @@ export default function SearchBar() {
         // });
 
         //with below line first get coordinates from first url and use second url.
-        axios.get(`http://localhost:8000/weather/`) 
+        axios.get(`http://localhost:8000/${name}/${unit}`) 
         .then((response) => {
         console.log(response.data);
         let data=response.data
-        temp([data.name, data.current.temp, data.current.weather[0].main, data.daily[0].temp.max, data.daily[0].temp.min, data.current.weather[0].icon, data.current.sunrise, data.current.sunset, data.current.dt, data.timezone, data.hourly[1].dt, data.hourly[1].pop, data.current.feels_like, data.daily[0].humidity, data.daily[0].wind_speed, data.hourly[0].visibility, data.daily[0].weather[0].icon, data.daily[0].pop, data.daily[1].dt, data.daily[1].temp.max, data.daily[1].temp.min, data.daily[1].weather[0].icon, data.daily[1].pop, data.daily[2].dt, data.daily[2].temp.max, data.daily[2].temp.min, data.daily[2].weather[0].icon, data.daily[2].pop, data.daily[3].dt, data.daily[3].temp.max, data.daily[3].temp.min, data.daily[3].weather[0].icon, data.daily[3].pop, data.daily[4].dt, data.daily[4].temp.max, data.daily[4].temp.min, data.daily[4].weather[0].icon, data.daily[4].pop]) 
+        temp([data[0].name, data[1].current.temp, data[1].current.weather[0].main, data[1].daily[0].temp.max, data[1].daily[0].temp.min, data[1].current.weather[0].icon, data[1].current.sunrise, data[1].current.sunset, data[1].current.dt, data[1].timezone, data[1].hourly[1].dt, data[1].hourly[1].pop, data[1].current.feels_like, data[1].daily[0].humidity, data[1].daily[0].wind_speed, data[1].hourly[0].visibility, data[1].daily[0].weather[0].icon, data[1].daily[0].pop, data[1].daily[1].dt, data[1].daily[1].temp.max, data[1].daily[1].temp.min, data[1].daily[1].weather[0].icon, data[1].daily[1].pop, data[1].daily[2].dt, data[1].daily[2].temp.max, data[1].daily[2].temp.min, data[1].daily[2].weather[0].icon, data[1].daily[2].pop, data[1].daily[3].dt, data[1].daily[3].temp.max, data[1].daily[3].temp.min, data[1].daily[3].weather[0].icon, data[1].daily[3].pop, data[1].daily[4].dt, data[1].daily[4].temp.max, data[1].daily[4].temp.min, data[1].daily[4].weather[0].icon, data[1].daily[4].pop]) 
         })
         .catch((err) => {
         console.log(err.message);
@@ -70,7 +71,7 @@ export default function SearchBar() {
             <span className="weatherApp">The Weather App</span>
             <span>
                 <input type="text" className="resizedTextBox"
-                    placeholder={"Search City or Zip Code"} id="searchCity" name="searchCity" onChange={handleInput}
+                    placeholder={"Search City or Zip Code"} id="id" name="q" onChange={handleInput}
                 />
                 <button className="resizedButton" onClick={handleSubmit}>Search</button>
             </span>
